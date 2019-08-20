05480a1ea086e560068bb9a561a6817820bf19bf
Michael Wilson <v-miwils@microsoft.com>
2019-08-20 10:15:19 -0700

    added stubbed functions to JS file and added basic info at the top of html/css/js files

---
+++ javascript/basic-web-developer/index.js
@@ -0,0 +1,25 @@
+/**
+ * @author Michael Wilson
+ * @description The goal of this is to really put my web development skills to the test.
+ * @version 1.0.0
+ */
+(function() {
+  function testPrototype() {}
+  function testClass() {}
+  function testIIFE() {}
+  function testClosure() {}
+  function testModulePattern() {}
+  function testHositing() {}
+  function testCurring() {}
+  function testMemoization() {}
+  function testApply() {}
+  function testBind() {}
+  function testCall() {}
+  function testPolymorphism() {}
+  function testCallback() {}
+  function testAJAX() {}
+  function testPromise() {}
+  function testAsyncAwait() {}
+  function testJSON() {}
+  function testDataStructures() {}
+})();
